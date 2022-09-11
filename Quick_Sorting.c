cda706d4bf9ff697c7fb44f663ed2cd176ab1038
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-09-11 08:24:56 +0530

    Quick Sorting
    An easy program of quick sort .

---
+++ Quick_Sorting.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+void printArray(int *A, int n)
+{
+    for (int i = 0; i < n; i++){
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+int partition(int A[], int low, int high){
+    int pivot = A[low];
+    int i = low + 1;
+    int j = high;
+    int temp;
+    do
+    {
+        while (A[i] <= pivot){
+            i++;
+        }
+        while (A[j] > pivot){
+            j--;
+        }
+        if (i < j){
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }       
+    }while (i < j);
+    // Swap A[low] and A[j]
+    temp = A[low];
+    A[low] = A[j];
+    A[j] = temp;
+    return j;
+}
+void quickSort(int A[], int low, int high){
+    int partitionIndex; // Index of pivot after partition
+    if (low < high){
+        partitionIndex = partition(A, low, high); 
+        quickSort(A, low, partitionIndex - 1);  // sort left subarray 
+        quickSort(A, partitionIndex + 1, high); // sort right subarray
+    }
+}
+int main(){
+
+    int A[] = {9, 4, 4, 8, 7, 5, 6};
+
+    int n = 9;
+    n =7;
+    printArray(A, n);
+    quickSort(A, 0, n - 1);
+    printArray(A, n);
+    return 0;
+}
