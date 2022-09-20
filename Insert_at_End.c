ba7b31747dfe5ced1e573a6c9b9980b23bd83ea3
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-09-20 06:16:20 +0530

    Insert at End
    A simple program which shows how to insert the new node and data at the end in linked list.

---
+++ Insert_at_End.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void Linked_list_Traversal(struct Node *head)
+{
+    while (head != NULL)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+struct Node *Insert_At_End(struct Node *node, int data)
+{
+    struct Node *ptr=(struct Node *)malloc(sizeof(struct Node));
+    struct Node *p=node;
+    ptr->data = data;
+    while(p->next!=NULL){
+        p=p->next;
+    }
+    p->next=ptr;
+    ptr->next=NULL;
+    return node;
+}
+int main()
+{
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *ptr_2 = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *ptr_3 = (struct Node *)malloc(sizeof(struct Node));
+    // Inserting the data;
+    head->data = 56;
+    ptr_2->data = 34;
+    ptr_3->data = 98;
+    // Connecting the node;
+    head->next = ptr_2;
+    ptr_2->next = ptr_3;
+    ptr_3->next = NULL;
+    //Traversal of the Linked List;
+    Linked_list_Traversal(head);
+    // Traversal of the Updated Linked List ;
+    head = Insert_At_End(head, 69);
+    printf("Updated Linked List:- \n");
+    Linked_list_Traversal(head);
+    return 69;
+}
