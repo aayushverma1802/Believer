c3ad64ad4df5cb1eecedee6e2504ed21e72d34ea
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-09-27 20:29:45 +0530

    Parenthesis Balancing
    Parenthesis Balancing using the stacks.

---
+++ Paranthesis_Balancing.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct stack
+{
+    char *arr;
+    int size;
+    int top;
+};
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void push(struct stack *ptr, int data)
+{
+    ptr->top++;
+    ptr->arr[ptr->top] = data;
+}
+void pop(struct stack *ptr)
+{
+    ptr->top--;
+}
+int Match(char *exp)
+{
+    struct stack *sp;
+    sp->size = 100;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sizeof(char) * sp->size);
+    for (int i = 0; exp[i] != '\0'; i++)
+    {
+        if (exp[i] == '(')
+        {
+            push(sp, '(');
+        }
+        else if (exp[i] == ')')
+        {
+            if (isEmpty(sp))
+            {
+                return 0;
+            }
+            pop(sp);
+        }
+    }
+    if (isEmpty(sp))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    char *exp = "((8)*(9))";
+    if (Match(exp))
+    {
+        printf("Balanced successfully \n");
+    }
+    else
+    {
+        printf("Not Balanced correctly\n");
+    }
+    return 0;
+}
