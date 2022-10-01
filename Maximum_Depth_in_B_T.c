3cab4388471be6c0bd547b3552eb6f501a33d09b
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-10-01 22:47:11 +0530

    Maximum Depth in Binary Tree
    A program showing the height of the binary tree using the recursion method.

---
+++ Maximum_Depth_in_B_T.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+struct Node *createNode(int data)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+void preOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+void postOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+void inOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+int isBst(struct Node *root)
+{
+    static struct Node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBst(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && prev->data >= root->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBst(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+int height(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        int l = height(root->left);
+        int r = height(root->right);
+        if (l > r)
+        {
+            return l + 1;
+        }
+        else
+        {
+            return r + 1;
+        }
+    }
+}
+int main()
+{
+    struct Node *p = createNode(5);
+    struct Node *p1 = createNode(3);
+    struct Node *p2 = createNode(6);
+    struct Node *p3 = createNode(1);
+    struct Node *p4 = createNode(4);
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    inOrder(p);
+    printf("\n");
+    if (isBst(p))
+    {
+        printf("This is a bst");
+    }
+    else
+    {
+        printf("This is not a bst");
+    }
+    printf("Depth of the Binary tree:-\n%d ", height(p));
+    return 0;
+}
