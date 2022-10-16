68b49dd965e675b59c7efe67a4dc25fff197f548
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-10-16 07:06:40 +0530

    Check for a Balanced Tree

---
+++ check_Balanced_BT.cpp
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+struct Node *createNode(int data)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+void preOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+void postOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+void inOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+int isBst(struct Node *root)
+{
+    static struct Node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBst(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && prev->data >= root->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBst(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+int height(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        int l = height(root->left);
+        int r = height(root->right);
+        if (l > r)
+        {
+            return l + 1;
+        }
+        else
+        {
+            return r + 1;
+        }
+    }
+}
+bool Checker(Node *root)
+{
+    if (root == NULL)
+    {
+        return true;
+    }
+    if (!Checker(root->left))
+    {
+        return false;
+    }
+    if (!Checker(root->right))
+    {
+        return false;
+    }
+    int l = height(root->left);
+    int r = height(root->right);
+    if (abs(l - r) <= 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+
+{
+    struct Node *p = createNode(5);
+    struct Node *p1 = createNode(3);
+    struct Node *p2 = createNode(6);
+    struct Node *p3 = createNode(2);
+    struct Node *p4 = createNode(4);
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    if(Checker(p)){
+        cout<<"Balanced Tree "<<"\n";
+
+    }
+    else{
+        cout<<"Unbalanced Tree";
+    }
+    return 0;
+}
