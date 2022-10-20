0d057faec20a33d31d221130e883813302f37b70
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-10-20 10:49:19 +0530

    Depth First Search

---
+++ DFS.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdio.h>
+int visited[7] = {0, 0, 0, 0, 0, 0, 0};
+int A[7][7] = {
+    {0, 1, 1, 1, 0, 0, 0},
+    {1, 0, 1, 0, 0, 0, 0},
+    {1, 1, 0, 1, 1, 0, 0},
+    {1, 0, 1, 0, 1, 0, 0},
+    {0, 0, 1, 1, 0, 1, 1},
+    {0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0}
+
+};
+
+void DFS(int i)
+{
+    printf("%d ", i);
+    visited[i] = 1;
+    for (int j = 0; j < 7; j++)
+    {
+        if (A[i][j] == 1 && !visited[j])
+        {
+            DFS(j);
+        }
+    }
+}
+int main()
+{
+    // DFS implementation
+    DFS(0);
+    return 69;
+}
