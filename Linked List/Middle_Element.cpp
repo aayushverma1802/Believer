f259127d7e9624d688afe41de66deccb50ef1db6
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-11-05 19:44:39 +0530

    Finding middle element in a linked list
    https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1?page=1&company[]=Amazon&category[]=Linked%20List&curated[]=1&sortBy=submissions

---
+++ Linked List/Middle_Element.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int traverse(Node * head){
+        int count=0;
+        Node *p=head;
+        while(p!=NULL){
+            p=p->next;
+            count++;
+            
+        }
+        return count;
+    }
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        int size=traverse(head);
+        int s=size/2;
+        int i=0;
+        while(i<s){
+            
+            head=head->next;
+            i++;
+            
+        }
+        return head->data;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
