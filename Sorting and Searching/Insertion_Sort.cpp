0e60e47ea505c7f48f179c913de8cc1830b94e76
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-11-18 20:01:32 +0530

    Insertion Sort
    https://practice.geeksforgeeks.org/problems/insertion-sort/1

---
+++ Sorting and Searching/Insertion_Sort.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    void insert(int arr[], int i)
+    {
+        // code here
+    }
+
+public:
+    // Function to sort the array using insertion sort algorithm.
+    void insertionSort(int a[], int n)
+    {
+        // code here
+        int j, key;
+        for (int i = 1; i <= n - 1; i++)
+        {
+            key = a[i];
+            j = i - 1;
+            while (j >= 0 && a[j] > key)
+            {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000], n, T, i;
+
+    scanf("%d", &T);
+
+    while (T--)
+    {
+
+        scanf("%d", &n);
+
+        for (i = 0; i < n; i++)
+            scanf("%d", &arr[i]);
+
+        Solution ob;
+        ob.insertionSort(arr, n);
+        printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
