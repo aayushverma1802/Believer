cc7bb946780dcfdb02dddf2b5b2dcb0bc496c88b
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-11-18 20:06:58 +0530

    Bubble Sort
    https://practice.geeksforgeeks.org/problems/bubble-sort/1

---
+++ Sorting and Searching/Bubble_Sort.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// C program for implementation of Bubble sort
+#include <stdio.h>
+
+// swapping the elements
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int a[], int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[1000], n, T, i;
+
+    scanf("%d", &T);
+
+    while (T--)
+    {
+
+        scanf("%d", &n);
+
+        for (i = 0; i < n; i++)
+            scanf("%d", &arr[i]);
+
+        Solution ob;
+
+        ob.bubbleSort(arr, n);
+        printArray(arr, n);
+    }
+    return 0;
+    ;
+}
+// } Driver Code Ends
