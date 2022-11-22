9a37499db943b9d5410812d1d1c71d40ef7c38f0
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-11-22 13:31:43 +0530

    Heap Deletion
    https://www.youtube.com/watch?v=NKJnHewiGdc

---
+++ Heaps/Heap_Deletion.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+class Heap
+{
+public:
+    int arr[100];
+    int size = 0;
+    Heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent], arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    void deletefromHeap()
+    {
+        if (size == 0)
+        {
+            cout << "Nothing to Delete";
+            return;
+        }
+        arr[1] = arr[size];
+        size--;
+
+        // Take roots to its correct position
+        int i = 1;
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
+            if (leftIndex < size and arr[i] < arr[leftIndex])
+            {
+                swap(arr[i], arr[leftIndex]);
+                i = leftIndex;
+            }
+            else if (rightIndex < size and arr[i] < arr[rightIndex])
+            {
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+};
+int main()
+{
+    Heap h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.print();
+    h.deletefromHeap();
+    h.print();
+    return 69;
+}
