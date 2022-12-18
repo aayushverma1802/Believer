88f5e3ca84716c2b4309707119ba01dde465da04
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2022-12-18 20:24:16 +0530

    Maximum sum of non-adjacent elements
    https://www.codingninjas.com/codestudio/problems/maximum-sum-of-non-adjacent-elements_843261?leftPanelTab=0

---
+++ Dynamic Programming/(Recursion)Maximum_sum_of_non_adjacent_elements.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+int solve(vector<int> &nums, int n)
+{
+    // Base case
+    if (n < 0)
+    {
+        return 0;
+    }
+    if (n == 0)
+    {
+        return nums[0];
+    }
+    int incl = solve(nums, n - 2) + nums[n];
+    int excl = solve(nums, n - 1) + 0;
+    return max(incl, excl);
+}
+int maximumNonAdjacentSum(vector<int> &nums)
+{
+    // Write your code here.
+    int n = nums.size();
+    int ans = solve(nums, n - 1);
+    return ans;
+}
