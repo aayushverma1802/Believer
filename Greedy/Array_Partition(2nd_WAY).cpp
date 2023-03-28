135da9372e734077480a544f45f2761bfb13cdd5
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2023-03-28 18:25:56 +0530

    Array Partition
    https://leetcode.com/problems/array-partition/description/

---
+++ Greedy/Array_Partition(2nd_WAY).cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int arrayPairSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+        vector<pair<int, int>> v;
+        for (int i = 0; i < n; i = i + 2)
+        {
+            // if(i%2==0){
+            ans += nums[i];
+            // }
+        }
+        return ans;
+    }
+};
