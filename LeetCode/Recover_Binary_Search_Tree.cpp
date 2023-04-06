e2a824395bdf7b8e3c391575d482c8f0826e3aad
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2023-04-06 10:46:33 +0530

    Recover Binary Search Tree
    https://leetcode.com/problems/recover-binary-search-tree/description/

---
+++ LeetCode/Recover_Binary_Search_Tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+    void inorder(TreeNode *root, vector<int> &vec)
+    {
+        if (!root)
+            return;
+        inorder(root->left, vec);
+        vec.push_back(root->val);
+        inorder(root->right, vec);
+    }
+    void solve(TreeNode *root, vector<int> &vec, int &i)
+    {
+        if (!root)
+            return;
+        // i++;
+        solve(root->left, vec, i);
+        if (root->val != vec[i])
+        {
+            root->val = vec[i];
+        }
+        i++;
+        solve(root->right, vec, i);
+    }
+
+public:
+    void recoverTree(TreeNode *root)
+    {
+        vector<int> vec;
+        inorder(root, vec);
+        sort(vec.begin(), vec.end());
+        int i = 0;
+        solve(root, vec, i);
+    }
+};
