a63cccb75c02348daaf7a90c5b24c47d428b9ab2
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2023-08-28 23:15:48 +0530

    Update  Number_of_Good_Pairs.cpp

--- Array/ Number_of_Good_Pairs.cpp
+++ Array/ Number_of_Good_Pairs.cpp
@@ -8,7 +8,7 @@ public:
     int n = nums.size ();
     int j = nums.size () - 1;
     for (int i = 0; i < nums.size (); i++)
-      {
+      { 
 	for (int j = i + 1; j < nums.size (); j++)
 	  {
 	    if (nums[i] == nums[j])
