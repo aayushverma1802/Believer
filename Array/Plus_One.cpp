d25e86d337148e37a88c80c3254d100a425c8815
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2023-09-07 23:20:27 +0530

    Plus One
    https://leetcode.com/problems/plus-one/description/?envType=featured-list&envId=top-interview-questions%3FenvType%3Dfeatured-list&envId=top-interview-questions

---
+++ Array/Plus_One.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int n = digits.size();
+        vector<int> v;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+        }
+        digits.push_back(0);
+        digits[0] = 1;
+        return digits;
+    }
+};
