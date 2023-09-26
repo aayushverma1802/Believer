de1559fec6dfac9ec8d9169cc2768f3f9f998ea8
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2023-09-26 17:13:55 +0530

    Remove Duplicate Letters
    https://www.youtube.com/watch?v=JyA858J7CrA&t=667s

---
+++ LeetCode/Remove_Duplicate_Letters.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    string removeDuplicateLetters(string s)
+    {
+        vector<int> lastIndex(26, 0);
+        for (int i = 0; i < s.length(); i++)
+        {
+            lastIndex[s[i] - 'a'] = i;
+        }
+        vector<bool> seen(26, false);
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            int curr = s[i] - 'a';
+            if (seen[curr])
+            {
+                continue;
+            }
+            while (st.size() > 0 and st.top() > s[i] and i < lastIndex[st.top() - 'a'])
+            {
+                seen[st.top() - 'a'] = false;
+                st.pop();
+            }
+            st.push(s[i]);
+            seen[curr] = true;
+        }
+        string res = "";
+        while (st.size() > 0)
+        {
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
