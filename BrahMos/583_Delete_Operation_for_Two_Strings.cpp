3a5d2e61518a3d7811723ba5d8e8812092b9d4f0
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-01-31 00:11:59 +0530

    Add files via upload

---
+++ BrahMos/583_Delete_Operation_for_Two_Strings.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int solve(string &t1, string &t2, int n, int m, int i, int j, vector<vector<int>> &dp)
+    {
+        if (i == n)
+        {
+            return 0;
+        }
+        if (j == m)
+        {
+            return 0;
+        }
+        if (dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        int ans = 0;
+        if (t1[i] == t2[j])
+        {
+            return ans = 1 + solve(t1, t2, n, m, i + 1, j + 1, dp);
+        }
+
+        ans = max(solve(t1, t2, n, m, i + 1, j, dp), solve(t1, t2, n, m, i, j + 1, dp));
+        return dp[i][j] = ans;
+    }
+
+    int minDistance(string s, string t)
+    {
+        int n = s.size();
+        // string t = s;
+        // reverse(t.begin(), t.end());
+        int m = t.size();
+        vector<vector<int>> dp(1000, vector<int>(1000, -1));
+        int ans = solve(s, t, n, m, 0, 0, dp);
+        return s.size() + t.size() - 2 * ans;
+    }
+};
