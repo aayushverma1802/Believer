25ac2a45c7959d85ab9e950e39b42a21536f6c6e
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-05-27 18:58:38 +0530

    Add files via upload

---
+++ BrahMos/Morris_Traversal.cpp
@@ -0,0 +1,63 @@
+//This code contains the preorder and the inorder
+
+//Inorder code
+vector<int>getInorder(TreeNode *root){
+    vector<int>inorder;
+    TreeNode *curr=root;
+    while(curr!=NULL){
+        if(curr->left==NULL){
+            inorder.push_back(curr->val);
+            curr=curr->right;
+        }
+        else{
+            TreeNode *prev=curr->left;
+            while(prev->right and prev->right!=curr){
+                prev=prev->right;
+
+            }
+            if(prev->right==NULL){
+                prev->right=curr;
+                curr=curr->left;
+            }
+            else{
+                prev->right=NULL;
+                inorder.push_back(curr->val);
+                curr=curr->right;
+
+            }
+        }
+    }
+    return inorder;
+}
+
+
+
+//Preorder code
+vector<int>getInorder(TreeNode *root){
+    vector<int>inorder;
+    TreeNode *curr=root;
+    while(curr!=NULL){
+        if(curr->left==NULL){
+            inorder.push_back(curr->val);
+            curr=curr->right;
+        }
+        else{
+            TreeNode *prev=curr->left;
+            while(prev->right and prev->right!=curr){
+                prev=prev->right;
+
+            }
+            if(prev->right==NULL){
+                prev->right=curr;
+                inorder.push_back(curr->val);
+                curr=curr->left;
+            }
+            else{
+                prev->right=NULL;
+                curr=curr->right;
+
+            }
+        }
+    }
+    return inorder;
+}
