e8318fe5aa1b21a09199b6077b40db58866f4204
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-05-29 22:04:26 +0530

    Add files via upload

---
+++ BrahMos/104_Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int ans(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        int l = ans(root->left);
+        int r = ans(root->right);
+        if (l > r)
+            return l + 1;
+        else
+            return r + 1;
+    }
+    int maxDepth(TreeNode *root)
+    {
+        return ans(root);
+    }
+};
