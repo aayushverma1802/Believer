c097fd2849687813dc34042ff3d328ba0bdd6108
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-05-30 23:50:24 +0530

    Add files via upload

---
+++ BrahMos/100_Same_Tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (p == NULL and q == NULL)
+        {
+            return true;
+        }
+        if (p == NULL and q != NULL)
+        {
+            return false;
+        }
+        if (p != NULL and q == NULL)
+        {
+            return false;
+        }
+        bool j = isSameTree(p->left, q->left);
+        bool k = isSameTree(p->right, q->right);
+        if (k == true and j == true and p->val == q->val)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
