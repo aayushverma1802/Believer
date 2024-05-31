925f0eafd0d2bf27fc107b3b603fdc24857ac6ee
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-05-31 22:04:02 +0530

    Add files via upload

---
+++ BrahMos/700_Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        while (root != NULL and root->val != val)
+        {
+            if (root->val > val)
+            {
+                root = root->left;
+            }
+            else
+            {
+                root = root->right;
+            }
+        }
+        return root;
+    }
+};
