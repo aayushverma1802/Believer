31f8039b339dac34abc4a585eb55816906518c0d
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-06-07 22:49:38 +0530

    Implementing_Dijkstra_Algorithm.cpp

--- Graphs/Implementing_Dijkstra_Algorithm.cpp
+++ Graphs/Implementing_Dijkstra_Algorithm.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class Solution
 {
-public:
+public: 
     // Function to find the shortest distance of all the vertices
     // from the source vertex S.
     vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
