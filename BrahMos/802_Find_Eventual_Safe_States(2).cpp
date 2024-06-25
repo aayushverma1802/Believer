03b557cdcd4ca2370311b1867346d47ac1f3eeb4
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-06-25 22:13:25 +0530

    Add files via upload

---
+++ BrahMos/802_Find_Eventual_Safe_States(2).cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>> &pp)
+    {
+        int V = pp.size();
+        queue<int> q;
+        vector<int> indegree(V, 0);
+        vector<int> adj[V];
+
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : pp[i])
+            {
+                adj[it].push_back(i);
+                // indegree[i]++;
+            }
+        }
+
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : pp[i])
+            {
+                // adj[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        for (int i = 0; i < V; i++)
+        {
+            if (indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while (q.empty() == false)
+        {
+            int node = q.front();
+            ans.push_back(node);
+            q.pop();
+            for (auto i : adj[node])
+            {
+                indegree[i]--;
+
+                if (indegree[i] == 0)
+                {
+                    q.push(i);
+                }
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
