ede5af9787d195c3d0acb53fc5b5a449bf2c5b2e
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-06-27 19:30:32 +0530

    Add files via upload

--- BrahMos/239_Sliding_Window_Maximum.cpp
+++ BrahMos/239_Sliding_Window_Maximum.cpp
@@ -1,3 +1,9 @@
+// When new element comes nums[i], make space for it (window size can't be greater than k)
+// 2. Now, when nums [i] comes, there is no need to keep small elements in that window, pop them
+
+// 3. Now push i in deque -> for nums [i]
+// 4. If (1 >= k-1), then deq.front() is our answer for that window
+
 class Solution
 {
 public:
