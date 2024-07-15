f746ae83611dc592cfd60ac8cb79a9aac9d1e3bc
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-07-15 19:00:19 +0530

    Add files via upload

---
+++ BrahMos/450_Delete_Node_in_a_BST.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *predecessor(TreeNode *root)
+    {
+        TreeNode *pred = root->left;
+        while (pred->right != NULL)
+        {
+            pred = pred->right;
+        }
+        return pred;
+    }
+    TreeNode *deleteNode(TreeNode *root, int key)
+    {
+        if (root == NULL)
+            return NULL;
+        if (root->val == key)
+        {
+            // case 1: No child
+            if (root->left == NULL && root->right == NULL)
+                return NULL;
+            // case 2: one child
+            else if (root->left == NULL || root->right == NULL)
+            {
+                if (root->left != NULL)
+                    return root->left;
+                else
+                    return root->right;
+            }
+            // case 3: two child
+            // replace the node with its inorder predecessor/sucessor
+            // after replacing delete the predecessor/sucessor
+            else
+            {
+                TreeNode *pred = predecessor(root);
+                root->val = pred->val;
+                root->left = deleteNode(root->left, pred->val);
+            }
+        }
+        else if (root->val > key)
+        { // go left
+            root->left = deleteNode(root->left, key);
+        }
+        else
+        { // go right
+            root->right = deleteNode(root->right, key);
+        }
+        return root;
+    }
+};
