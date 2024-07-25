654fe030b40bd20415634ed053f6b0700d624203
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-07-25 22:37:28 +0530

    Add files via upload

---
+++ BrahMos/144_Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void solve(TreeNode *root, vector<int> &ans)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        ans.push_back(root->val);
+
+        solve(root->left, ans);
+        solve(root->right, ans);
+    }
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return {};
+        }
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
