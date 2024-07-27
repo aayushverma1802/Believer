de8ac3cdad002a2e2d42ac123307410300090fe3
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-07-27 15:07:34 +0530

    Add files via upload

---
+++ BrahMos/124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int solve(TreeNode *root, int &ans)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        int l = max(0, solve(root->left, ans));
+        int r = max(0, solve(root->right, ans));
+        ans = max(ans, l + r + root->val);
+        return max(l, r) + root->val;
+    }
+    int maxPathSum(TreeNode *root)
+    {
+        int ans = INT_MIN;
+        solve(root, ans);
+        return ans;
+    }
+};
