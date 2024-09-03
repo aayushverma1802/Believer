7a6fa84a69b91e5f20dcb8695a84202d23e8b882
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-09-03 19:24:05 +0530

    Add files via upload

---
+++ BrahMos/1002_Find_Common_Characters.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    vector<string> commonChars(vector<string> &words)
+    {
+        int n = words.size();
+        vector<vector<int>> freq(n, vector<int>(26, 0));
+        for (int i = 0; i < n; i++)
+        {
+            string curr = words[i];
+            for (auto &c : curr)
+            {
+                freq[i][c - 'a']++;
+            }
+        }
+        vector<string> ans;
+        for (int i = 0; i < 26; i++)
+        {
+            int mn = INT_MAX;
+            for (int j = 0; j < n; j++)
+            {
+                mn = min(mn, freq[j][i]);
+            }
+            while (mn > 0)
+            {
+                ans.push_back(string(1, i + 'a'));
+                mn--;
+            }
+        }
+        return ans;
+    }
+};
