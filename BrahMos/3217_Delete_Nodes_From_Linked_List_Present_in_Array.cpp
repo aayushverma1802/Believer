07bf94e931cec307b1b4c3ac2a6fd1a312c6db92
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-09-06 20:39:44 +0530

    Add files via upload

---
+++ BrahMos/3217_Delete_Nodes_From_Linked_List_Present_in_Array.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *modifiedList(vector<int> &nums, ListNode *head)
+    {
+        unordered_set<int> st(nums.begin(), nums.end());
+        while (head != nullptr and st.find(head->val) != st.end())
+        {
+            ListNode *temp = head;
+            head = head->next;
+            delete (temp);
+        }
+        ListNode *curr = head;
+        while (curr != NULL and curr->next != NULL)
+        {
+            if (st.find(curr->next->val) != st.end())
+            {
+                ListNode *temp = curr->next;
+                curr->next = curr->next->next;
+                delete (temp);
+            }
+            else
+            {
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
