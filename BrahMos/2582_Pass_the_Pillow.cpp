9dce4fef554efa6c1fbc501e27218e2c4f9c2267
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-09-21 00:36:20 +0530

    Add files via upload

---
+++ BrahMos/2582_Pass_the_Pillow.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int passThePillow(int n, int time)
+    {
+        int direction = 1; // move in the forward
+        int ans = 1;       // Store the ans;
+        // ans=1 because atleast one person is there
+        while (time > 0)
+        {
+            ans += direction;
+            if (ans == n)
+            {
+                direction = -1;
+            }
+            else if (ans == 1)
+            {
+                direction = 1;
+            }
+            time--;
+        }
+        return ans;
+    }
+};
