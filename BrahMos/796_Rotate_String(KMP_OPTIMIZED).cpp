da84980bdd5aa8b955ee327e5da8c43cc2fb114d
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2024-11-03 22:03:41 +0530

    Add files via upload

---
+++ BrahMos/796_Rotate_String(KMP_OPTIMIZED).cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool rotateString(string s, string goal)
+    {
+        int n = s.length();
+
+        for (int i = 0; i < n; i++)
+        {
+            rotate(s.begin(), s.begin() + 1, s.end());
+            if (s == goal)
+                return true;
+        }
+        return false;
+    }
+};
+/*
+Rotate stl format
+ rotate(s.begin(), s.begin() + 1, s.end());
+
+ (starting range, which position will become starting range,end of the range)
+
+
+ this stl uses KMP which we had studied in DAA
+ TC O(n)
+ SC O(n)
+*/
