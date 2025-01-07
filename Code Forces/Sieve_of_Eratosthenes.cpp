db928391f44e3d540736806b44dc0dae69587a04
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2025-01-07 10:42:37 +0530

    Add files via upload

---
+++ Code Forces/Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<int> sieveOfEratosthenes(int N)
+    {
+        // Write Your Code here
+        int n=N;
+        vector<int>prime(N+1,1);
+        for(int i=2;i<n;i++){
+            if(prime[i]==1){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j]=0;
+                }
+            }
+        }
+        vector<int>ans;
+        for(int i=2;i<=n;i++){
+            if(prime[i]==1){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        vector<int> primes  = ob.sieveOfEratosthenes(N);
+        for(auto prime : primes) {
+            cout<< prime <<" ";
+        }
+        cout<<endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
