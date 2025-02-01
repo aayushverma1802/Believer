019fbc33d9237e24febf17ac179916c450b5d793
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2025-02-01 11:48:02 +0530

    Add files via upload

---
+++ Code Forces/Alternating_Current.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int>st;
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(!st.empty() and st.top()==s[i])
+            st.pop();
+
+        else
+            st.push(s[i]);
+    }
+    st.empty()?puts("Yes"):puts("No");
+    return 0;
+}
