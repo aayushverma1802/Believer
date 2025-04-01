0aa283173e1def04c21d5a4212ab64f281612866
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2025-04-01 21:58:18 +0530

    Add files via upload

---
+++ BrahMos/886_Possible_Bipartition.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    bool bip(int n, vector<vector<int>> &adj, int node, vector<int> &color)
+    {
+
+        for (auto i : adj[node])
+        {
+            if (color[i] == -1)
+            {
+                color[i] = !color[node];
+                if (!bip(n, adj, i, color))
+                    return false;
+            }
+            else if (color[i] == color[node])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool possibleBipartition(int n, vector<vector<int>> &dislikes)
+    {
+        vector<vector<int>> adj(n + 1);
+        for (auto &pair : dislikes)
+        {
+            adj[pair[0]].push_back(pair[1]);
+            adj[pair[1]].push_back(pair[0]);
+        }
+
+        vector<int> color(n + 1, -1);
+        for (int i = 1; i < n; i++)
+        {
+            if (color[i] == -1)
+            {
+                if (bip(n, adj, i, color) == false)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
